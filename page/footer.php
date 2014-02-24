f90a8884d25242f67064561bc08999da5953a336
dario vilardi <vilardid@rto1dcvm159.rete.poste>
2014-02-24 18:02:57 +0100

    fix header e footer

--- page/footer.php
+++ page/footer.php
@@ -1,13 +1,14 @@
 	<div id="footer">
 		<div class="centrato">
 			<ul>
-				<li class="box_footer"><a href="#"> Terms</a></li>
-				<li class="box_footer"><a href="#"> Privacy</a></li>
-				<li class="box_footer"><a href="http://10.200.22.24/ADC/ReportGiornaliero.aspx"> ADC</a></li>
-				<li class="box_footer"><a href="http://10.200.22.24/rfid/"> R-fid</a></li>
-				<li class="box_footer"><a href="#"> Credits</a></li>
-				<li class="w3c"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" /></li>
-				<li class="w3c"><img src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!" /></li>
+				<li class="box_footer"><a href="/">/</a></li>
+				<li class="box_footer"><a href="../accessi">Accessi</a></li>
+				<li class="box_footer"><a href="../guardiania/privacy/">Privacy</a></li>
+				<li class="box_footer"><a href="http://rto1y11c020.rete.poste/">Sicurezza</a></li>
+				<li class="box_footer"><a href="http://rge1pats101/orario">Orario</a></li>
+				<li class="box_footer"><a href="http://10.200.22.24/ADC/ReportGiornaliero.aspx">ADC</a></li>
+				<li class="box_footer"><a href="http://10.200.22.24/rfid/">R-fid</a></li>
+				<li class="box_footer"><a href="#">Wiki</a></li>
 			</ul>
 		</div>
 	</div> <!-- end div footer -->
--- page/header.php
+++ page/header.php
@@ -1,6 +1,6 @@
 <div class="header">
 	<img class="logo_poste" title ="Poste Italiane" src="img/logo_poste.png" alt="logo Poste Italiane" />
-	<img class="badge" src="img/footer_badges.png" alt="DC-TO badge" />
+	<img class="logo_frazionario" src="img/logo_frazionario.png" alt="TI/GSI/GI/TOW" />
 	<a href="?page=home">
 		<img title ="clicca per tornare alla home" class="logo_title" src="img/logo_title.png" alt="gestione magazzino logo" />
 	</a>
