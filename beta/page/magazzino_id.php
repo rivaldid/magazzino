7b875ed7a766835eaf265c63eec50e82ff344075
Dario Vilardi <vilardid@posteitaliane.it>
2014-10-20 10:16:06 +0200

    id in magazzino

--- beta/page/magazzino_id.php
+++ beta/page/magazzino_id.php
@@ -7,7 +7,7 @@ if (!$conn) die('Errore di connessione: '.mysql_error());
 $dbsel = mysql_select_db('magazzino', $conn);
 if (!$dbsel) die('Errore di accesso al db: '.mysql_error());
 
-$query = "SELECT * FROM vserv_magazzino;";
+$query = "SELECT * FROM vserv_magazzino_id;";
 $res = mysql_query($query);
 if (!$res) die('Errore nell\'interrogazione del db: '.mysql_error());
 
@@ -15,8 +15,9 @@ $a = "";
 
 //print
 $a .= "<table>\n";
-$a .= "<caption>STATO MAGAZZINO (<a href=\"?page=magazzino_id\">ID</a>)</caption>\n";
+$a .= "<caption>STATO MAGAZZINO (<a href=\"?page=magazzino\">NOID</a>)</caption>\n";
 $a .= "<thead><tr>\n";
+	$a .= "<th>ID</th>\n";
 	$a .= "<th>TAGS</th>\n";
 	$a .= "<th>Quantita'</th>\n";
 	$a .= "<th>Lista posizioni</th>\n";
