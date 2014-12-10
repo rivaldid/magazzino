dab6f861157eab978fd23d273e0fb174a09133be
Dario Vilardi <vilardid@posteitaliane.it>
2014-12-10 16:38:19 +0100

    working @ vserv_ng

--- 020/page/vserv_ng.php
+++ 020/page/vserv_ng.php
@@ -21,11 +21,9 @@
  *	[/HEADPHP]
  * 
  * 
- * 
  * 	[BODYPHP]
  * 	[/BODYPHP]
  *
- * 
  * 
  * 	[FOOTPHP]
  * 		ritorno contenuti
@@ -39,9 +37,11 @@
 
 
 
+// ******** HEADPHP ****************************************************
 
 // inizializzo variabili
-$bypassaformselect = false;
+$a = "";
+$log = "";
 
 // inizializzo risorse
 if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
@@ -49,23 +49,64 @@ $log .= remesg("Autenticato come ".$_SERVER["AUTHENTICATE_UID"],"msg");
 
 
 
-// test task iniziato
-if (isset($_SESSION['begin'])) {
+// ******** BODYPHP ****************************************************
+
+// if not begin
+if (!(isset($_SESSION['begin']))) {
 	
-	$bypassaformselect = true;
+	// test se ho input valido
+	if ((isset($_POST['modifica']) OR (isset($_POST['scarica']))) AND (!empty($_POST['check_list']))) {
 		
+		// quindi copia l'input dal post
+		foreach ($_POST['check_list'] as $i => $j) {
+		
+			$_SESSION['id_merce'][$j] = $_POST['id_merce'][$j];
+			$_SESSION['tags'][$j] = $_POST['tags'][$j];
+			$_SESSION['posizioni'][$j] = $_POST['posizioni'][$j];
+			$_SESSION['tot'][$j] = $_POST['tot'][$j];
+		
+		}
+		
+		// begin true
+		$_SESSION['begin'] = true;
+	
+	// altrimenti visualizzo merce
+	} else {
+		
+		$log .= remesg("Magazzino in visualizzazione merce","msg");
+		vserv_magazzino_select();
+		
+	}
+
+// altrimenti se begin inizializzato
 } else {
 	
-	$_SESSION['scarico']
+	// se ho dato stop resetto
+	if (isset($_POST['stop'])) {
+		
+		$log .= remesg($msg9,"msg");
+	
+		// reset $_SESSION
+		$_SESSION = array();
+		session_unset();
+		session_destroy();
+
+		/* generate new session id and delete old session in store */
+		session_regenerate_id(true);
+		if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
+		
+	} else {
+	
+		// copio tutto il post
+		foreach ($_POST AS $key => $value) $_SESSION[$key] = $value;
+	
+	}
 	
 }
-	
 
 
 
-
-
-
+// ******** FOOTPHP ****************************************************
 
 // ritorno contenuti
 $a .= $_SESSION['contents'];
