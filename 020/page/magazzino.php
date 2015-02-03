f082dcf68f945c6120983319c29a8be4bca1f6a8
Dario Vilardi <vilardid@posteitaliane.it>
2015-02-03 12:34:17 +0100

    single page magazzino

--- 020/page/magazzino.php
+++ 020/page/magazzino.php
@@ -2,28 +2,39 @@
 
 logging2(occhiomalocchio(basename(__FILE__)),accesslog);
 
+
+// inizializzo risorse
+
+// variabili
 $a = "";
 $log = "";
 
-//begin mysql
+// mysql
 $conn = mysql_connect('localhost','magazzino','magauser');
 if (!$conn) die('Errore di connessione: '.mysql_error());
 
 $dbsel = mysql_select_db('magazzino', $conn);
 if (!$dbsel) die('Errore di accesso al db: '.mysql_error());
 
-$query = "SELECT * FROM vserv_magazzino;";
+
+// interrogazione
+if (isset($_GET["id"])) $query = "SELECT * FROM vserv_magazzino_id;";
+else $query = "SELECT * FROM vserv_magazzino;";
+
 $res = mysql_query($query);
 if (!$res) die('Errore nell\'interrogazione del db: '.mysql_error());
 
-//print
+
+// risultati
 $a .= jsxtable;
 $a .= jsaltrows;
 $a .= "<table class='altrowstable' id='alternatecolor'>\n";
 
-$log .= remesg("Visualizzazione con <a href=\"?page=magazzino_id\">ID</a>","msg");
+if (isset($_GET["id"])) $log .= remesg("Visualizzazione senza <a href=\"?page=magazzino\">ID</a>","msg");
+else $log .= remesg("Visualizzazione con <a href=\"?page=magazzino&id\">ID</a>","msg");
 
 $a .= "<thead><tr>\n";
+	if (isset($_GET["id"])) $a .= "<th>ID</th>\n";
 	$a .= "<th>TAGS</th>\n";
 	$a .= "<th>Posizioni con parziali</th>\n";
 	$a .= "<th>Tot</th>\n";
@@ -39,6 +50,8 @@ while ($row = mysql_fetch_array($res, MYSQL_NUM)) {
 
 $a .= "</tbody>\n</table>\n";
 
+
+// stampo
 echo "<div id=\"log\">\n";
 echo remesg("Notifiche","tit");
 echo remesg("Autenticato come ".$_SERVER["AUTHENTICATE_UID"]." alle ".date('H:i')." del ".date('d/m/Y'),"msg");
@@ -53,7 +66,8 @@ echo $a;
 
 mysql_free_result($res);
 
-// end mysql
+
+// termino risorse
 mysql_close($conn);
 
 ?>
