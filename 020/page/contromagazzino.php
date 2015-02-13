1f69c9d04b4d0d168a96852ebd3d0a6da6f60e8c
Dario Vilardi <vilardid@posteitaliane.it>
2015-02-13 11:22:01 +0100

    contromagazzino

--- 020/page/contromagazzino.php
+++ 020/page/contromagazzino.php
@@ -16,17 +16,17 @@ if (!$conn) die('Errore di connessione: '.mysql_error());
 $dbsel = mysql_select_db('magazzino', $conn);
 if (!$dbsel) die('Errore di accesso al db: '.mysql_error());
 
-// log
-if (isset($_GET["id"])) {
-	$query = "SELECT * FROM vserv_magazzino_id;";
-	$log .= remesg("Visualizzazione senza <a href=\"?page=magazzino\">ID</a>","msg");
-} else {
-	$query = "SELECT * FROM vserv_magazzino;";
-	$log .= remesg("Visualizzazione con <a href=\"?page=magazzino&id\">ID</a>","msg");
-}
+$log .= remesg("Torna alla <a href=\"?page=magazzino\">visualizzazione magazzino</a>","msg");
 
-$log .= remesg("Aggiornamento <a href=\"?page=aggiornamento_magazzino\">posizione o quantita'</a> in magazzino","msg");
-$log .= remesg("<a href=\"?page=contromagazzino\">Contromagazzino</a>","msg");
+// query
+$query = "SELECT MERCE.tags AS merce,sum(seljoin.quantita) AS tot,GROUP_CONCAT(DISTINCT CONCAT(seljoin.posizione,'(',seljoin.quantita,')')) AS destinazioni FROM
+(SELECT sel1.id_merce,sel1.quantita,sel1.posizione FROM
+(SELECT id_merce,quantita,posizione FROM OPERAZIONI JOIN REGISTRO USING(id_registro) WHERE direzione=0 AND contatto!='Aggiornamento') AS sel1 
+LEFT JOIN 
+(SELECT id_merce,quantita,posizione FROM MAGAZZINO WHERE quantita>0) AS sel2 
+ON CONCAT(sel1.id_merce,sel1.quantita,sel1.posizione)=CONCAT(sel2.id_merce,sel2.quantita,sel2.posizione)
+WHERE CONCAT(sel2.id_merce,sel2.quantita,sel2.posizione) IS NULL) AS seljoin
+JOIN MERCE ON seljoin.id_merce=MERCE.id_merce GROUP BY MERCE.tags ORDER BY MERCE.tags";
 
 // interrogazione
 $res = mysql_query($query);
@@ -39,9 +39,9 @@ $a .= jsaltrows;
 $a .= "<table class='altrowstable' id='alternatecolor'>\n";
 $a .= "<thead><tr>\n";
 	if (isset($_GET["id"])) $a .= "<th>ID</th>\n";
-	$a .= "<th>TAGS</th>\n";
-	$a .= "<th>Posizioni con parziali</th>\n";
-	$a .= "<th>Tot</th>\n";
+	$a .= "<th>MERCE</th>\n";
+	$a .= "<th>Quantita</th>\n";
+	$a .= "<th>Destinazioni</th>\n";
 $a .= "</tr></thead>\n";
 $a .= "<tbody>\n";
 
