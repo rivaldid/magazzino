68ec7f0087d68d3f7f7363d25ce88eb6c45bb8ba
Dario Vilardi <vilardid@posteitaliane.it>
2015-05-21 14:29:32 +0200

    installato nuovo logging su db

--- 020/page/carico.php
+++ 020/page/carico.php
@@ -1,7 +1,14 @@
 <?php
 
-logging2(occhiomalocchio(basename(__FILE__)),accesslog);
-
+// occhiomalocchio
+$conn = mysql_connect('localhost','magazzino','magauser');
+if (!$conn) die('Errore di connessione: '.mysql_error());
+$dbsel = mysql_select_db('magazzino', $conn);
+if (!$dbsel) die('Errore di accesso al db: '.mysql_error());
+if (!(isset($_SERVER['HTTP_REFERER']))) $_SERVER['HTTP_REFERER'] = null;
+$logging = "CALL input_trace('{$_SERVER['REQUEST_TIME']}','{$_SERVER['REQUEST_URI']}','{$_SERVER['HTTP_REFERER']}','{$_SERVER['REMOTE_ADDR']}','{$_SERVER['REMOTE_USER']}','{$_SERVER['PHP_AUTH_USER']}','{$_SERVER['HTTP_USER_AGENT']}');";
+mysql_query($logging);
+mysql_close($conn);
 
 /*
  * carico merce in magazzino, script frontend per stored procedure
@@ -345,7 +352,6 @@ if ($DEBUG) {
 	if (isset($iposizione)) $log .= remesg("Inserimento posizione: ".$iposizione,"debug");
 	if (isset($sposizione)) $log .= remesg("Suggerimento posizione: ".$sposizione,"debug");
 }
-
 
 
 // 2. test bottoni
--- 020/page/magazzino_ng.php
+++ 020/page/magazzino_ng.php
@@ -1,7 +1,14 @@
 <?php
 
-logging2(occhiomalocchio(basename(__FILE__)),accesslog);
-
+// occhiomalocchio
+$conn = mysql_connect('localhost','magazzino','magauser');
+if (!$conn) die('Errore di connessione: '.mysql_error());
+$dbsel = mysql_select_db('magazzino', $conn);
+if (!$dbsel) die('Errore di accesso al db: '.mysql_error());
+if (!(isset($_SERVER['HTTP_REFERER']))) $_SERVER['HTTP_REFERER'] = null;
+$logging = "CALL input_trace('{$_SERVER['REQUEST_TIME']}','{$_SERVER['REQUEST_URI']}','{$_SERVER['HTTP_REFERER']}','{$_SERVER['REMOTE_ADDR']}','{$_SERVER['REMOTE_USER']}','{$_SERVER['PHP_AUTH_USER']}','{$_SERVER['HTTP_USER_AGENT']}');";
+mysql_query($logging);
+mysql_close($conn);
 
 // inizializzo risorse
 
@@ -18,7 +25,6 @@ if (!$conn) die('Errore di connessione: '.mysql_error());
 
 $dbsel = mysql_select_db('magazzino', $conn);
 if (!$dbsel) die('Errore di accesso al db: '.mysql_error());
-
 
 // log
 $query = "SELECT * FROM vista_magazzino_ng_full;";
