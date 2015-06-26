3aa5f04d96ca827c5c91a345f3e2f77756c49cbb
Dario Vilardi <vilardid@posteitaliane.it>
2015-06-26 13:42:21 +0200

    fix descrizioni home

--- page/home.php
+++ page/home.php
@@ -4,6 +4,11 @@
 
 $a = ""; $log = "";
 
+$log .= remesg("In questa applicazione sono previste due modalita' di accesso: in <mark>LETTURA</mark>* ed in <mark>SCRITTURA</mark>","info");
+$log .= remesg("Le pagine in acquisizione dati sono provviste di due modalita': <mark>MANUALE</mark>* e <mark>GUIDATA</mark>","info");
+$log .= remesg("*L'accesso in <mark>LETTURA</mark> e' privato delle pagine che agiscono attivamente sul database","info");
+$log .= remesg("*La modalita' di acquisizione <mark>MANUALE</mark> ha priorita' su quella <mark>GUIDATA</mark>","info");
+
 $a .= "<a href=\"/magazzino\"><img border=\"0\" src=\"imgs/maga.png\" alt=\"Homepage\" /></a>\n";
 
 echo makepage($a, $log);
