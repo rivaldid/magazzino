1833e6016024a202ebf5cf8a53936dbbb3cb7218
Dario Vilardi <vilardid@posteitaliane.it>
2015-07-20 16:15:12 +0200

    cleanup

--- page/sviluppo.php
+++ page/sviluppo.php
@@ -2,10 +2,23 @@
 <hr>
 
 <div style="text-align:justify; padding:20px 120px;">
-	<p>Il lavoro si divide in due componenti: una web ed una database.</p>
-	<p>La componente web utilizza script php per elaborare informazioni trasportate in post e consegnate alle sessioni, ogni sessione si chiude automaticamente quando l'operazione in corso si e' conclusa con esito positivo.</p>
-	<p>La componente database utilizza strutture mysql come stored function e stored procedure.
-	Essa e' organizzata ad oggetti e dispone di interfacce sia pubbliche, tramite le quali parla con la componente web, sia con interfacce private tramite le quali mette in comunicazione tabelle e funzionalita'.</p>
-	<p>Il progetto e' stato supportato dal personale del datacenter di Torino e sviluppato da</p>
-	<p style="text-align:center"><i class="fa fa-code"> <a href="mailto:VILARDID@posteitaliane.it">Dario Vilardi</a></i> e <i class="fa fa-instagram"> <a href="mailto:DALES177@posteitaliane.it">Davide D'Alessio</a></i></p>
+<p>
+	<b>IL MAGAZZINO</b> nasce dall'esigenza di tracciare il materiale in arrivo presso il datacenter di torino, esigenza che si configura in particolar modo 
+	nello stoccaggio di materiali per scopi futuri. Nasce da un lavoro del gruppo accessi del datacenter di torino, i quali erano improntati però alla gestione 
+	materiale della merce, la gestione logica è stata affidata a questo progetto. Il lavoro si divide in due sotto progetti: uno che si occupa della parte dati, 
+	ed un altro che si occupa della parte interattiva.
+</p>
+<p>
+	<b>L'IDEA</b> è lasciare il carico di lavoro alla parte dati, quindi inserimenti risposte eccezioni con i dati saranno trasparenti alla parte interattiva, 
+	la quale riceverà solo stream di dati. in ogni fase di lavoro è sempre marcata la natura di separare le due componenti.
+</p>
+<p>
+	<b>LO SVILUPPO</b> ha seguito un approccio per prototipi successivi, formalizzata un'attività e relativi casi d'uso si è passati ad implementazioni minimali da
+	sottoporre alle attività designate.
+</p>
+<p>
+	<b></b>
+</p>
+
 </div>
+<p style="text-align:center"><i class="fa fa-code"> <a href="mailto:VILARDID@posteitaliane.it">Dario Vilardi</a></i> e <i class="fa fa-instagram"> <a href="mailto:DALES177@posteitaliane.it">Davide D'Alessio</a></i></p>
