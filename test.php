96d8d62fff25d7aed7d028123810f5c86fb40b18
vilardid <vilardid@posteitaliane.it>
2016-01-25 16:31:23 +0100

    working again

---
+++ test.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<!--[if IE 7]><html lang="it" class="ie7"><![endif]-->
+<!--[if IE 8]><html lang="it" class="ie8"><![endif]-->
+<!--[if IE 9]><html lang="it" class="ie9"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><html lang="it"><![endif]-->
+<!--[if !IE]><html lang="it-IT"><![endif]-->
+
+<head>
+<title>Gestione Magazzino</title>
+<link rel="stylesheet" href="/lib/bower_components/dynatable/jquery.dynatable.css" type="text/css" />
+<link rel="stylesheet" href="/lib/bower_components/font-awesome-bower/css/font-awesome.css" type="text/css" />
+<link rel="text/javascript" src="/lib/bower_components/jquery/dist/jquery.js"></script>
+<link rel="text/javascript" src="/lib/bower_components/dynatable/jquery.dynatable.js"></script>
+</head>
+
+<body>
+<?php
+	if ($_SERVER["QUERY_STRING"] != NULL) {
+		if (!empty($_GET["page"])) $page = sprintf("page/%s.php",$_GET["page"]);
+		if (!file_exists($page)) $page = sprintf("page/404.php");
+	} else $page = sprintf("page/home.php");
+
+	require_once 'lib/init.php';
+	include 'lib/menu.php';
+	echo jsxtop;
+
+	$db = myquery::start();
+	if (!(isset($_SERVER['HTTP_REFERER']))) $_SERVER['HTTP_REFERER'] = null;
+	myquery::logger($db);
+	
+	$query = myquery::magazzino($db);
+
+	//presentation
+	$a = "<table>\n";
+	
+	$a .= "<thead>\n";
+	$a .= "<th data-dynatable-column=\"merce\">Merce</th>\n";
+	$a .= "<th data-dynatable-column=\"posizione\">Posizione</th>\n";
+	$a .= "<th data-dynatable-column=\"quantita\">Quantita'</th>\n";
+	$a .= "</thead>\n";
+	
+	$a .= "<tbody>\n";
+	foreach ($query as $row) {
+		$riga .= "<tr>\n";
+		$riga .= "<td>".$row['merce']."</td>\n";
+		$riga .= "<td>".$row['posizione']."</td>\n";
+		$riga .= "<td>".$row['quantita']."</td>\n";
+		$riga .= "</tr>\n";
+	}
+	$a .= $riga;
+	$a .= "</tbody>\n";
+	
+	$a .= "</table>\n";
+	
+	echo $a;
+	
+	include 'lib/footer.php';
+	
+?>
+
+</body>
+
+</html>
