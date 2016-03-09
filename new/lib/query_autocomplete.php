a7881ab40d3668fe2dff3d0646c15f9885541155
vilardid <vilardid@posteitaliane.it>
2016-03-09 20:03:43 +0100

    working

--- new/lib/query_autocomplete.php
+++ new/lib/query_autocomplete.php
@@ -12,19 +12,23 @@ class autocomplete extends DB {
 		}
 	}
 
-	public function tipi_doc($db) {
+	public function tipi_doc($db,$term) {
 
 		try {
-			return $query = $db->query("SELECT * FROM vserv_tipodoc;")->resultset();
+			return $query = $db->query("SELECT * FROM vserv_tipodoc WHERE label LIKE ?")
+				->bind(1,$term.'%')
+				->resultset();
 		} catch (PDOException $e) {
 			error_handler($e->getMessage());
 		}
 	}
 
-	public function num_doc($db) {
+	public function num_doc($db,$term) {
 
 		try {
-			return $query = $db->query("SELECT * FROM vserv_numdoc;")->resultset();
+			return $query = $db->query("SELECT * FROM vserv_numdoc WHERE numero LIKE ?")
+				->bind(1,$term.'%')
+				->resultset();
 		} catch (PDOException $e) {
 			error_handler($e->getMessage());
 		}
